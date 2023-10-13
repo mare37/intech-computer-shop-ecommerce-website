493ce21d68ba29887ce829d12627f24d96a06548
Wilson Mare <marewilson35@gmail.com>
2023-10-13 15:00:50 +0300

    issue/20

---
+++ server/src/models/cartModel.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+import { Cart } from "../types";
+
+// Declare the Schema of the Mongo model
+var cartSchema = new mongoose.Schema<Cart>(
+  {
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        count: Number,
+        color: String,
+        price: Number,
+      },
+    ],
+    cartTotal: Number,
+    totalAfterDiscount: Number,
+    orderby: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//Export the model
+export default mongoose.models.Cart ||   mongoose.model("Cart", cartSchema);
