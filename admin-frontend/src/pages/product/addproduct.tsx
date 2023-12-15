fb8f632afb66a387bd562b3b9701b9076ce277f4
Wilson Mare <marewilson35@gmail.com>
2023-12-15 15:10:44 +0300

    issue/53

--- admin-frontend/src/pages/product/addproduct.tsx
+++ admin-frontend/src/pages/product/addproduct.tsx
@@ -1,107 +1,246 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useAppDispatch, useAppSelector } from "../../hooks";
 import * as yup from "yup";
+import { ToastContainer, toast } from "react-toastify";
 import Dropzone from "react-dropzone";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
 import styles from "./product.module.scss";
-import { useState } from "react";
-import Item from "antd/es/list/Item";
+import tableStyles from "../table.module.scss";
+
+import { reset,resetGettingProducts } from "../../redux/productSlice";
+
+import { addProduct, getOneProduct, updateProduct } from "../../api/product";
+import { getAllBrands } from "../../api/brand";
+import { getAllColours } from "../../api/colour";
+import { getAllProductCategories } from "../../api/productcatetory";
 
 const schema = yup.object().shape({
   title: yup.string().required(),
-  description: yup.string().required(),
+  quantity: yup.number().required(),
   brand: yup.string().required(),
   price: yup.number().required(),
   category: yup.string().required(),
   colour: yup.string().required(),
-  statusCategory: yup.string().required(),
+  tag: yup.string().required(),
 });
 
-const DATA = [
-  {
-    sNo: 1,
-    title: "samsung",
-  },
-  {
-    sNo: 2,
-    title: "lenovo",
-  },
-];
-
-const handleChange = () => {};
-
 function isQuillEmpty(value: string) {
-    if (value.replace(/<(.|\n)*?>/g, '').trim().length === 0 && !value.includes("<img")) {
-       return true;
-     }
-       return false;
-    }
+  if (
+    value.replace(/<(.|\n)*?>/g, "").trim().length === 0 &&
+    !value.includes("<img")
+  ) {
+    return true;
+  }
+  return false;
+}
+
+interface brand {
+  title: string;
+  id: string;
+}
+
+interface category {
+  title: string;
+  id: string;
+}
+
+type Product = {
+  _id: string;
+  title: string;
+  brand: brand;
+  quantity: number;
+  category: category;
+  colour: string;
+  price: number;
+};
+
+const price: number | string = 0;
+const brand: string | brand = "";
+const category: string | category = "";
 
 function AddProduct() {
   const [acceptedFiles, setAcceptedFile] = useState<File[]>([]);
+  const [brands, setBrands] = useState<any>([]);
+  const [colours, setColours] = useState<any>([]);
+  const [productCategories, setProductCategories] = useState<any>([]);
+  const [description, setDescription] = useState("");
+
+  const notify = () => toast.success("Product added Successfully!");
+
+  //Notify if there was an error during delection. :)
+  const notifyError = () => toast.error("Failed to add product!");
+
+  const { productIsLoading, productIsError, productIsSuccess, gettingProducts } = useAppSelector(
+    (state) => state.product
+  );
+
+  const dispatch = useAppDispatch();
 
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(schema),
   });
 
+  useEffect(() => {
+    getAllBrands(dispatch)
+      .then((response) => {
+        console.log(response.result);
+        console.log("triggered");
+
+        setBrands(response.result.reverse());
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    getAllColours(dispatch)
+      .then((response) => {
+        console.log(response.result);
+       
+
+        setColours(response.result.reverse());
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    getAllProductCategories(dispatch)
+      .then((response) => {
+        console.log(response.result);
+
+        setProductCategories(response.result.reverse());
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    dispatch(reset());
+  }, []);
+
+  
+  
+
+  useEffect(() => {
+   
+
+    dispatch(reset());
+  
+  }, [productIsSuccess, productIsError]);
+
   return (
     <div
-      onSubmit={handleSubmit((data) => console.log(data))}
+      onSubmit={handleSubmit(
+        (data: any) => {
+          const isempty = isQuillEmpty(description);
+
+          if (isempty) {
+            console.log("Description is empty");
+          } else {
+            data["description"] = description;
+            addProduct(data, dispatch).then((response)=>{
+              console.log(response);
+              if(response.productCreated ){
+                notify()
+              }else{
+                notifyError()
+              }
+              
+            })
+           
+          }
+        },
+        (error) => {
+          console.log(error);
+        }
+      )}
       className={styles.product}
     >
       <div className={styles.productContainer}>
+        <ToastContainer theme="light" />
         <form>
-          <h3>Add Product</h3>
-          <input {...register("title")} placeholder="Enter product title" />
+          <h1>Add Product</h1>
+          <input
+            type="text"
+            {...register("title")}
+            placeholder="Enter product title"
+          />
           {errors.title && <p>Brand is required.</p>}
 
-          <input {...register("price")} placeholder="Enter price" />
+          <input
+            type="number"
+            {...register("price")}
+            placeholder="Enter price"
+          />
           {errors.price && <p>Price is required.</p>}
+
+          <input
+            type="number"
+            {...register("quantity")}
+            placeholder="Enter quantity"
+          />
+          {errors.quantity && <p>Quantity is required.</p>}
+
           <ReactQuill
-          
             theme="snow"
             style={{ width: "100%", marginBottom: "10px" }}
             placeholder="Enter description"
-            onChange={handleChange}
+            onChange={(value) => {
+              setDescription(value);
+            }}
           />
 
           <select {...register("brand")}>
             <option value="">Select Brand</option>
-            {DATA.map((item) => {
-              return <option key={item.sNo}>{item.title}</option>;
+            {brands.map((item: any) => {
+              return (
+                <option value={item._id} key={item._id}>
+                  {item.title}
+                </option>
+              );
             })}
           </select>
           {errors.brand && <p>Brand is required.</p>}
 
           <select {...register("category")}>
             <option value="">Select product category</option>
-            {DATA.map((item) => {
-              return <option key={item.sNo}>{item.title}</option>;
+            {productCategories.map((item: any) => {
+              return (
+                <option value={item._id} key={item._id}>
+                  {item.title}
+                </option>
+              );
             })}
           </select>
           {errors.category && <p>Product category is required.</p>}
 
-
-          <select {...register("statusCategory")}>
-            <option value="">Select Status category</option>
+          <select {...register("tag")}>
+            <option value="">Select Tag</option>
             <option value="featured">Featured</option>
             <option value="popular">Popular</option>
             <option value="special">Special</option>
           </select>
-          {errors.statusCategory && <p>Status category is required.</p>}
+          {errors.tag && <p>Status category is required.</p>}
 
           <select {...register("colour")}>
             <option value="">Select colour</option>
-            {DATA.map((item) => {
-              return <option key={item.sNo}>{item.title}</option>;
+            {colours.map((item: any) => {
+              return (
+                <option value={item._id} key={item._id}>
+                  {item.title}
+                </option>
+              );
             })}
           </select>
+
           {errors.colour && <p>Price is required.</p>}
 
           <Dropzone
@@ -117,7 +256,11 @@ function AddProduct() {
 
                   {acceptedFiles.length > 0 &&
                     acceptedFiles.map((item) => {
-                      return <p className={styles.imageItems}>{"IMAGE "+item.name}</p>;
+                      return (
+                        <p className={styles.imageItems}>
+                          {"IMAGE " + item.name}
+                        </p>
+                      );
                     })}
                   {acceptedFiles.length === 0 && (
                     <p>
@@ -129,7 +272,17 @@ function AddProduct() {
             )}
           </Dropzone>
 
-          <button>Add Product</button>
+          <button disabled={productIsLoading}>Add Product</button>
+          {productIsSuccess && (
+            <span className={tableStyles.success}>
+              Product Created successfully.
+            </span>
+          )}
+          {productIsError && (
+            <span className={tableStyles.error}>
+              Something went wrong.Try again
+            </span>
+          )}
         </form>
       </div>
     </div>
--- admin-frontend/src/pages/productCategory/productcategory.tsx
+++ admin-frontend/src/pages/productCategory/productcategory.tsx
@@ -1,18 +1,34 @@
-
-
-
-
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
+import { useAppDispatch, useAppSelector } from "../../hooks";
+
+import {
+  addProductCategory,
+  getOneProductCategory,
+  updateProductCategory,
+} from "../../api/productcatetory";
+
 import styles from "./productcategory.module.scss";
+import tableStyles from "../table.module.scss";
 
 const schema = yup.object().shape({
-  colour: yup.string().required(),
+  productCategory: yup.string().required(),
 });
 
-function  ProductCategory() {
+function ProductCategory() {
+  const [productCategory, setProductCategory] = useState("");
+  const [productCatRetrieval, setProductCatRetrieval] = useState(false);
+
+  const { id } = useParams();
+
+  const dispatch = useAppDispatch();
+  const { isLoading, isError, isSuccess } = useAppSelector(
+    (state) => state.productCategory
+  );
   const {
     register,
     handleSubmit,
@@ -21,17 +37,58 @@ function  ProductCategory() {
     resolver: yupResolver(schema),
   });
 
+  useEffect(() => {
+    if (id != undefined) {
+      getOneProductCategory(id, dispatch)
+        .then((response) => {
+          console.log(response.result);
+          setProductCatRetrieval(true);
+          setProductCategory(response.result.title);
+        })
+        .catch((error) => {
+          setProductCatRetrieval(false);
+        });
+    }
+
+    /// dispatch(reset());
+  }, []);
+
   return (
     <div
-      onSubmit={handleSubmit((data) => console.log(data))}
-      className={styles.addcolor}
+      onSubmit={handleSubmit((data) => {
+        if (id === undefined) {
+        
+          addProductCategory(data.productCategory, dispatch);
+        } else {
+          setProductCatRetrieval(false);
+          updateProductCategory(id, productCategory, dispatch);
+        }
+      })}
+      className={styles.addProductCategory}
     >
-      <div    className={styles.addcolorContainer}  >
+      <div className={styles.addProductCategoryContainer}>
         <form>
-           <h3>Add product category</h3>
-          <input {...register("colour")}  />
-          {errors.colour && <p>Product category is required.</p>}
-          <button>Add product category</button>
+          <h3>{id === undefined ? "Add" : "Edit"} product category</h3>
+          <input
+           value={productCategory}
+            {...register("productCategory")}
+            onChange={(e) => {
+              setProductCategory(e.target.value);
+            }}
+          />
+          {errors.productCategory && <p>Product category is required.</p>}
+          <button>{id === undefined ? "Add" : "Edit"}</button>
+          {isSuccess === true && productCatRetrieval === false && (
+            <span className={tableStyles.success}>
+              Product category {id !== undefined ? "updated" : "created"}{" "}
+              successfully.
+            </span>
+          )}
+          {isError && (
+            <span className={tableStyles.error}>
+              Something went wrong.Try again
+            </span>
+          )}
         </form>
       </div>
     </div>
